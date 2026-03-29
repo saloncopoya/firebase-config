f712fef2a7db4758912fed2572577b52c8f071b3
saloncopoya <151898433+saloncopoya@users.noreply.github.com>
2026-03-28 21:27:13 -0600

    Update r.js

--- r.js
+++ r.js
@@ -1,13 +1,23 @@
-// ==============================================
-// CONFIGURACIÓN FIREBASE PARA ROOSTER (ADMIN)
-// ==============================================
-window.FIREBASE_ROOSTER_CONFIG = {
-    apiKey: "AIzaSyBMON_hBlUJfE-_L6qrt8MEP6JZrculRcs",
-    authDomain: "premiumcotejosgallistico-59e76.firebaseapp.com",
-    databaseURL: "https://premiumcotejosgallistico-59e76-default-rtdb.firebaseio.com",
-    projectId: "premiumcotejosgallistico-59e76",
-    storageBucket: "premiumcotejosgallistico-59e76.firebasestorage.app",
-    messagingSenderId: "785051078042",
-    appId: "1:785051078042:web:8e749ba849c5e7e5407e23"
-};
-console.log("✅ Firebase Rooster Config cargado");
+
+const dominiosPermitidos = [
+    'cmbt-2211-94b-omega.blogspot.com',
+    'legadoavicola.blogspot.com'
+];
+
+const dominioActual = window.location.hostname;
+const dominioPermitido = dominiosPermitidos.includes(dominioActual);
+
+if (dominioPermitido) {
+    window.FIREBASE_ROOSTER_CONFIG = {
+        apiKey: "AIzaSyBMON_hBlUJfE-_L6qrt8MEP6JZrculRcs",
+        authDomain: "premiumcotejosgallistico-59e76.firebaseapp.com",
+        databaseURL: "https://premiumcotejosgallistico-59e76-default-rtdb.firebaseio.com",
+        projectId: "premiumcotejosgallistico-59e76",
+        storageBucket: "premiumcotejosgallistico-59e76.firebasestorage.app",
+        messagingSenderId: "785051078042",
+        appId: "1:785051078042:web:8e749ba849c5e7e5407e23"
+    };
+    console.log('✅ Servidor Rooster conectado.');
+} else {
+    window.FIREBASE_ROOSTER_CONFIG = null;
+}
