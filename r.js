2e594f605c79bff9655315f4c3d363546eaabc48
saloncopoya <151898433+saloncopoya@users.noreply.github.com>
2026-03-28 21:55:49 -0600

    Update r.js

--- r.js
+++ r.js
@@ -1,10 +1,10 @@
-const dominiosPermitidosRooster = [
+var dominiosPermitidosRooster = [
     'cmbt-2211-94b-omega.blogspot.com',
     'legadoavicola.blogspot.com'
 ];
 
-const dominioActualRooster = window.location.hostname;
-const dominioPermitidoRooster = dominiosPermitidosRooster.includes(dominioActualRooster);
+var dominioActualRooster = window.location.hostname;
+var dominioPermitidoRooster = dominiosPermitidosRooster.includes(dominioActualRooster);
 
 if (dominioPermitidoRooster) {
     window.FIREBASE_ROOSTER_CONFIG = {
@@ -16,7 +16,8 @@ if (dominioPermitidoRooster) {
         messagingSenderId: "785051078042",
         appId: "1:785051078042:web:8e749ba849c5e7e5407e23"
     };
-    console.log('✅ Servidor Rooster.');
+    console.log('✅ Servidor Rooster conectado.');
 } else {
     window.FIREBASE_ROOSTER_CONFIG = null;
+    console.warn('🚫 Dominio no autorizado para Rooster:', dominioActualRooster);
 }
